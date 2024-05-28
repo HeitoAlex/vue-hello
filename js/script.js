0970eb3f39e1de5160ab1eb036a44cac7b429557
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-05-28 18:44:22 +0200

    bonus special

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ createApp({
         return {
         message: 'Supercalifragilisticexpialidocious',
         imageUrl: 'https://ongood.eu/wp-content/uploads/2020/01/supercalifragilistichespiralidoso_124328_display.jpg'
-
+        }
+    },
+    methods:{
+        stop: function(){
+            alert('Stop touching ME! >:(')
         }
     }
 }).mount('#app')
 
 
-// Supercalifragilisticexpialidocious!
-// Even though the sound of it is something quite atrocious
-// If you say it loud enough, you'll always sound precocious!
-// Supercalifragilisticexpialidocious!
