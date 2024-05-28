5cd86aa9c487281bbd8e140b50ba2e27386295ff
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-05-28 15:33:47 +0200

    bonus 1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-        message: 'Supercalifragilisticexpialidocious'
+        message: 'Supercalifragilisticexpialidocious',
+        imageUrl: 'https://ongood.eu/wp-content/uploads/2020/01/supercalifragilistichespiralidoso_124328_display.jpg'
+
         }
     }
 }).mount('#app')
+
+
+// Supercalifragilisticexpialidocious!
+// Even though the sound of it is something quite atrocious
+// If you say it loud enough, you'll always sound precocious!
+// Supercalifragilisticexpialidocious!
